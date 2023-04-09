77a045611f9e836531ccda14232174af356cdd98
TMillsap04 <tmmillsap@student.fullsail.edu>
2023-04-09 15:57:53 -0500

    Setting up Sanity CMS.

--- ecommerce/lib/client.js
+++ ecommerce/lib/client.js
@@ -1,14 +1,14 @@
-import sanityClient from '@sanity/client';
+import {createClient} from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-export const client = sanityClient({
-  projectId: 'vfxfwnaw',
+export const client = createClient({
+  projectId: 'dp5n6j24',
   dataset: 'production',
-  apiVersion: '2022-03-10',
+  apiVersion: '2023-04-04',
   useCdn: true,
   token: process.env.NEXT_PUBLIC_SANITY_TOKEN
 });
 
-const builder = imageUrlBuilder(client);
+export const builder = imageUrlBuilder(client);
 
 export const urlFor = (source) => builder.image(source);
